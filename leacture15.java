caee037e239b402d53fa3710792bc548fa63cc39
Swapnil Gavali <146269131+Swapnil-CS775@users.noreply.github.com>
2024-08-10 15:41:11 +0530

    Update leacture15.java

--- leacture15.java
+++ leacture15.java
@@ -69,3 +69,53 @@ public class Floore {
     }
 }
 //Outupu=9;
+
+//Binary search in infinite array
+package Swapnil.com;
+
+public class InfiniteArrya {
+    public static void main(String[] args) {
+        int []arr={1,3,8,12,13,15,19,22,41,46,58,65,68,70,73,79,80,81,89};
+        System.out.println(ans(arr,8));
+
+    }
+    static int ans(int []arr,int target)
+    {
+        int start=0;
+        int end=1; //we select box of two element initially
+        //when this while condition become false ..in that chunk our key is present ,,at that value of start and end
+        while(target>arr[end])
+        {
+            int temp=end+1;//basically this is our new start but we cant change here bcz we require this value below to find end
+            //doubble the size of box
+            //end=previous end + previous box size into 2
+            end=end+(end-start+1)*2;
+            start=temp;//here we define our start
+        }
+        //using above while loop we just find our chunk in which our key is belongs
+        //and now transfer this chunk with start, end,target, arr to the binary search function for searching
+        return BinarySearch(arr,target,start,end);
+    }
+
+    static int BinarySearch(int []arr,int target,int start,int end)
+    {
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target)
+            {
+                return 1;
+            }
+            else if(arr[mid]<target)
+            {
+                start=mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    
+}
+//output 1;
